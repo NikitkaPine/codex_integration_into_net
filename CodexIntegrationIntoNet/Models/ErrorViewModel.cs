730edb9bc3f22be09cb0427dc3655574bdecda24
NikitkaPine <n1k1tkayolka@gmail.com>
2026-02-18 10:33:14 +0200

    remove solution into repository

---
+++ CodexIntegrationIntoNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodexIntegrationIntoNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
